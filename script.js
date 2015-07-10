55d6e1d5928913dd759fc08a65fbd5dbed9319ba
brandon932 <bthornton_932@yahoo.com>
2015-07-10 17:04:24 -0600

    inital commit contains html file with scripts to complete loop problems

---
+++ script.js
@@ -0,0 +1,25 @@
+for (var i=0;i<=1000;i+=100){
+    console.log(i);
+};
+for (var i=1;i<=128;i*=2){
+  console.log(i);
+};
+for (var i=0;i<=10;i+=2){
+  console.log(i);
+};
+for (var i=0;i<=15;i+=3){
+  console.log(i);
+};
+for (var i=9;i<=0;i--){
+  console.log(i)
+};
+for (var i=1;i<4;i++){
+    for(var x=0;x<4;x++){
+          console.log(i);
+            };
+};
+for (var i=0;i<3;i++){
+  for (var x=0;x<=4;x++){
+    console.log(x);
+  };
+};
